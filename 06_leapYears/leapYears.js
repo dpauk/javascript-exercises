0c833cac6cbe9e888c071539e46a09c100652710
David Albone <dpauk@users.noreply.github.com>
2022-06-08 14:31:21 +0100

    Completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Is it divisible by 4?
+    if (year % 4 !== 0) {
+        return false;
+    }
 
+    // Is it divisble by 100, but not by 400?
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false
+    }
+
+    return true;
 };
 
 // Do not edit below this line
