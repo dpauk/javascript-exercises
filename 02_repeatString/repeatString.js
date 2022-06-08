31df9d5a70887be0cb029b104c9bcfb273520460
David Albone <dpauk@users.noreply.github.com>
2022-06-08 12:03:37 +0100

    Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numberOfRepeats) {
+    if (numberOfRepeats < 0) {
+        return "ERROR";
+    }
 
+    let output = "";
+    for (let i = 0; i < numberOfRepeats; i++) {
+        output += stringToRepeat;
+    }
+    return output;
 };
 
 // Do not edit below this line
