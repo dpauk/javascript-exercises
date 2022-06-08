1995eec622c05ca26d8a0d5b710e0301ffa4e3ee
David Albone <dpauk@users.noreply.github.com>
2022-06-08 14:40:45 +0100

    Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * (5.0 / 9.0)) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return Math.round(((celsius * (9.0 / 5.0)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
