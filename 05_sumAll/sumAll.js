5e66a87131226cdb44c0ddb7a08f885500f3177a
David Albone <dpauk@users.noreply.github.com>
2022-06-08 14:25:29 +0100

    Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    if (startNum < 0 || endNum < 0) {
+        return "ERROR"
+    }
+
+    if (typeof startNum !== 'number' || typeof endNum !== 'number') {
+        return "ERROR"
+    }
+
+    let lowestNum = startNum;
+    let highestNum = endNum;
+
+    if (endNum < startNum) {
+        lowestNum = endNum;
+        highestNum = startNum;
+    }
+
+    for (let n = lowestNum; n <= highestNum; n++) {
+        sum += n;
+    }
+    return sum;
 };
 
 // Do not edit below this line
