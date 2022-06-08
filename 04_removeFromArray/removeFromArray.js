4de285caa4a9efd3f6fd2633daa348bf91c661e6
David Albone <dpauk@users.noreply.github.com>
2022-06-08 12:47:57 +0100

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayIn, elementToRemove) {
+    let startingArray = arguments[0];
+    let elementsToRemove = [];
 
+    // Get all the elements to remove, ignoring the first argument
+    for (let i in arguments) {
+        if (i == 0) {
+            continue;
+        }
+        elementsToRemove.push(arguments[i]);
+    }
+
+    let output = [];
+
+    for (let item in startingArray) {
+        // Check to see if the current item should be removed
+        if (elementsToRemove.indexOf(startingArray[item]) == -1) {
+            output.push(startingArray[item]);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
