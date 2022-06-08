21e6f199095c561bae090bd6eba08e93ea506b12
David Albone <dpauk@users.noreply.github.com>
2022-06-08 12:25:03 +0100

    Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let arrayString = stringToReverse.split("");
+    let reversedArrayString = arrayString.reverse();
+    let output = "";
+
+    for (let i = 0; i < arrayString.length; i++) {
+        output += reversedArrayString[i];
+    }
+    return output;
 
 };
 
